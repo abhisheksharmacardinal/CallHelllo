03cbe1b315cf9ef4a59f804a6971fb1bba70617c
TestUser <demouser@Abhisheks-MacBook-Air.local>
2018-02-09 17:55:20 -0500

    Amend Added

---
+++ CloudAmendSDK/CloudAmendSDK.h
@@ -0,0 +1,19 @@
+//
+//  CloudAmendSDK.h
+//  CloudAmendSDK
+//
+//  Created by TestUser on 2/9/18.
+//  Copyright © 2018 TACT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CloudAmendSDK.
+FOUNDATION_EXPORT double CloudAmendSDKVersionNumber;
+
+//! Project version string for CloudAmendSDK.
+FOUNDATION_EXPORT const unsigned char CloudAmendSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CloudAmendSDK/PublicHeader.h>
+
+
